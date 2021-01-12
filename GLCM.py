6f8a2c9bd34a9e226c2b064301f7913394e5232f
MrPanch <gleb.panchenko.2013@gmail.com>
2021-01-12 14:50:35 +0300

    GLCM-based logic regression to detect suitable places

--- GLCM.py
+++ GLCM.py
@@ -9,96 +9,111 @@ from skimage.feature import greycomatrix, greycoprops
 from sklearn.linear_model import LogisticRegression
 
 
-def crop_samples(folder=f'GLCM_images/original/', sq_lin=100):
-    for image_name in tqdm(os.listdir(folder)):
-        image = cv2.imread(os.path.join(folder, image_name))
-        sdim_1 = image.shape[0] % sq_lin
-        sdim_2 = image.shape[1] % sq_lin
+def prepare_data(sq_lin=100):
 
-        sum_list = []
-        coord_list = []
-        for i in range(sdim_1, image.shape[0], sq_lin):
-            for j in range(sdim_2, image.shape[1], sq_lin):
-                image_slice = image[i:i + sq_lin, j:j + sq_lin]
-                image_slice = image_slice.flatten()
-                slice_sum = np.sum(image_slice)
-                sum_list.append(slice_sum)
-                coord_list.append([i, j])
+    for folder in ['train_negative', 'train_positive', 'validation']:
+        root_folder = os.path.join('GLCM_images', f'{folder}_class')
+        print(f'\n cropping samples for {folder}')
+        for image_name in tqdm(os.listdir(root_folder)):
+            image = cv2.imread(os.path.join(root_folder, image_name))
+            sdim_1 = image.shape[0] % sq_lin
+            sdim_2 = image.shape[1] % sq_lin
 
-        zip_iterator = zip(sum_list, coord_list)
-        sum_dic = dict(zip_iterator)
-        # помеять ревёрс, чтобы брать самые большие энтропии
-        sum_list.sort(reverse=True)
-        result = []
-        for sum_val in sum_list[:8]:
-            result.append(sum_dic[sum_val])
+            sum_list = []
+            coord_list = []
+            for i in range(sdim_1, image.shape[0], sq_lin):
+                for j in range(sdim_2, image.shape[1], sq_lin):
+                    image_slice = image[i:i + sq_lin, j:j + sq_lin]
+                    image_slice = image_slice.flatten()
+                    slice_sum = np.sum(image_slice)
+                    sum_list.append(slice_sum)
+                    coord_list.append([i, j])
 
-        # fig, maxs = plt.subplots(1, 1, figsize=(12, 8))
-        # maxs.imshow(image[:, :, 0], cmap='gray')
-        # maxs.axis('off')
-        # for i in range(len(result)):
-        #     rect2 = patches.Rectangle((result[i][0], result[i][1]),
-        #                               100, 100,
-        #                               linewidth=1, edgecolor='r', facecolor='none')
-        #     maxs.add_patch(rect2)
-        # plt.show()
+            zip_iterator = zip(sum_list, coord_list)
+            sum_dic = dict(zip_iterator)
+            if folder == 'train_negative':
+                sum_list.sort()
+            if folder == 'train_positive':
+                sum_list.sort(reverse=True)
+            result = []
+            for sum_val in sum_list[:8]:
+                result.append(sum_dic[sum_val])
 
-        idx = 0
-        for coord in result:
-            image_slice = image[coord[0]:coord[0] + sq_lin, coord[1]:coord[1] + sq_lin]
-            cv2.imwrite(f'GLCM_images/cropped/{image_name}_{idx}.png', image_slice)
-            idx += 1
+            idx = 0
+            for coord in result:
+                image_slice = image[coord[0]:coord[0] + sq_lin, coord[1]:coord[1] + sq_lin]
+                cv2.imwrite(f'GLCM_images/{folder}_cropped/{image_name}_{idx}.png', image_slice)
+                idx += 1
 
 
 def glcm_features():
-    correlation = []
-    dissimilarity = []
-    bottom_patches = []
+    result_negative_list = []
+    result_positive_list = []
+    result_validation_list = []
+    for folder in ['train_negative_cropped', 'train_positive_cropped', 'validation_cropped']:
+        correlation = []
+        dissimilarity = []
+        bottom_patches = []
+        root_folder = os.path.join(f'GLCM_images/', folder)
+        for image_name in os.listdir(root_folder):
+            image = cv2.imread(os.path.join(root_folder, image_name))
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            bottom_patches.append(image)
+        for patch in (bottom_patches):
+            glcm = greycomatrix(patch, distances=[5], angles=[0], levels=256,
+                                symmetric=True, normed=True)
+            correlation.append(greycoprops(glcm, 'correlation')[0, 0])
+            dissimilarity.append(greycoprops(glcm, 'dissimilarity')[0, 0])
 
-    for image_name in os.listdir(f'GLCM_images/cropped/'):
-        image = cv2.imread(os.path.join(f'GLCM_images/cropped/', image_name))
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        bottom_patches.append(image)
-    for patch in (bottom_patches):
-        glcm = greycomatrix(patch, distances=[5], angles=[0], levels=256,
-                            symmetric=True, normed=True)
-        correlation.append(greycoprops(glcm, 'correlation')[0, 0])
-        dissimilarity.append(greycoprops(glcm, 'dissimilarity')[0, 0])
-        # create the figure
+        if folder == 'train_negative_cropped':
+            result_negative_zip = zip(correlation, dissimilarity)
+            result_negative_list = list(result_negative_zip)
+        if folder == 'train_positive_cropped':
+            result_positive_zip = zip(correlation, dissimilarity)
+            result_positive_list = list(result_positive_zip)
+        if folder == 'validation_cropped':
+            result_validation_zip = zip(correlation, dissimilarity)
+            result_validation_list = list(result_validation_zip)
 
-        # display original image with locations of patches
-        # ax = fig.add_subplot(3, 2, 1)
-        # ax.imshow(image, cmap=plt.cm.gray,
-        #           vmin=0, vmax=255)
-        # ax.set_xlabel('Original Image')
-        # ax.set_xticks([])
-        # ax.set_yticks([])
-        # ax.axis('image')
-
-    # for each patch, plot (dissimilarity, correlation)
-    # fig = plt.figure(figsize=(8, 8))
-    # ax = fig.add_subplot(1, 1, 1)
-    # ax.plot(correlation[:len(bottom_patches)], dissimilarity[:len(bottom_patches)], 'go',
-    #         label='Bottom')
-    # ax.set_xlabel('GLCM Dissimilarity')
-    # ax.set_ylabel('GLCM Correlation')
-    # ax.legend()
-    #
-    # plt.tight_layout()
-    # plt.show()
-    result_zipping = zip(correlation, dissimilarity)
-    result = list(result_zipping)
+    result = {'negative': np.array(result_negative_list), 'positive': np.array(result_positive_list),
+              'validation': np.array(result_validation_list)}
     return result
 
 
-def log_regression(glcm_features):
-    y = np.ones((glcm_features.shape[0],1))
-    print(y.shape)
-    print(glcm_features.shape)
-    clf = LogisticRegression(random_state=0).fit(glcm_features, y)
+def log_regressor(glcm_features):
+    y_positive = np.ones(glcm_features['positive'].shape[0])
+    y_negative = np.zeros(glcm_features['negative'].shape[0])
+    X = np.append(glcm_features['positive'], glcm_features['negative'], axis=0)
+    y = np.append(y_positive, y_negative)
+    clf = LogisticRegression(random_state=0).fit(X, y)
+    return clf
+
+
+def clf_validation(clf, glcm_features):
+    X = glcm_features['validation']
+    prediction = clf.predict(X)
+    root_folder = os.path.join(f'GLCM_images', f'validation_cropped')
+    data_2_display = list(zip(os.listdir(root_folder), prediction))
+    for idx in range(len(data_2_display)//8):
+        fig = plt.figure(figsize=(9, 13))
+        columns = 4
+        rows = 2
+        ax = []
+
+        for i in range(columns * rows):
+            img = cv2.imread(os.path.join(root_folder, data_2_display[idx*8 + i][0]))
+            # create subplot and append to ax
+            ax.append(fig.add_subplot(rows, columns, i + 1))
+            ax[-1].set_title("image class: " + str(data_2_display[idx*8 + i][1]))  # set title
+            plt.imshow(img)
+
+        plt.show()  # finally, render the plot
+
+
 
 
 if __name__ == '__main__':
+    prepare_data()
     features = glcm_features()
-    features = np.array(features)
-    log_regression(features)
+    clf = log_regressor(features)
+    clf_validation(clf, features)
